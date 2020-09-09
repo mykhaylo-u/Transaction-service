df3299e472f82ecbd5d85e1ba738cbb7648db512
Mykhaylo Mykhaylo <u.myhaylo@gmail.com>
2020-09-09 13:56:55 +0300

    DB Migration was added

---
+++ transaction-service/transaction-service.database/Migrations/20200909102724_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace transaction_service.database.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 50, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(9, 2)", nullable: false),
+                    Currency = table.Column<string>(maxLength: 3, nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
--- transaction-service/transaction-service.web/wwwroot/js/site.js
+++ transaction-service/transaction-service.web/wwwroot/js/site.js
@@ -4,3 +4,7 @@ $(".custom-file-input").on("change", function () {
     var fileName = $(this).val().split("\\").pop();
     $(".custom-file-label").addClass("selected").html(fileName);
 });
+
+$(".custom-file-input").on("click", function () {
+    $(".text-success").text('');
+});
